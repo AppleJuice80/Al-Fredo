ab02a1a835cba5fdda5cca80ea2d37d726bbb84d
AppleJuice80 <ajandeska64@icloud.com>
2026-04-06 15:02:44 -0400

    Added backpack

--- src/main/java/alfredo/items/ModItems.java
+++ src/main/java/alfredo/items/ModItems.java
@@ -13,9 +13,9 @@ import net.minecraft.util.Identifier;
 public class ModItems {
     public static final Item THRUST_BOMB = registerItem("thrust_bomb");
     public static final Item PLANT_BOMB = registerItem("plant_bomb");
-
     public static final Item NEW_AND_IMPROVED_STATE_OF_THE_ART_LIFE_VEST = registerItem(
             "new_and_improved_state_of_the_art_life_vest");
+    public static final Item BACKPACK = registerItem("backpack");
 
     private static Item registerItem(String name) {
         Identifier id = Identifier.of(AlFredo.MOD_ID, name);
@@ -31,6 +31,7 @@ public class ModItems {
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(entries -> {
             entries.add(THRUST_BOMB);
             entries.add(NEW_AND_IMPROVED_STATE_OF_THE_ART_LIFE_VEST);
+            entries.add(BACKPACK);
         });
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(PLANT_BOMB);
