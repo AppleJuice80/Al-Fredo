95c700b534d83584f69b4e582955e4400a3ba004
AppleJuice80 <ajandeska64@icloud.com>
2026-04-10 08:59:11 -0400

    Fixed trajectories

--- src/main/java/alfredo/entities/custom/BackpackProjectileEntity.java
+++ src/main/java/alfredo/entities/custom/BackpackProjectileEntity.java
@@ -1,45 +1,37 @@
 package alfredo.entities.custom;
 
 import alfredo.entities.ModEntities;
-//import alfredo.items.ModItems;
-
+import alfredo.items.ModItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ExplosiveProjectileEntity;
-
-//import net.minecraft.item.ItemStack;
-
-//import net.minecraft.util.hit.BlockHitResult;
-//import net.minecraft.util.hit.EntityHitResult;
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
-//import net.minecraft.world.explosion.Explosion;
 
-public class BackpackProjectileEntity extends ExplosiveProjectileEntity {
-	private World world;
-
-
-    public BackpackProjectileEntity(EntityType<? extends ExplosiveProjectileEntity> entity_type, World world) {
+public class BackpackProjectileEntity extends ThrownItemEntity {
+	public BackpackProjectileEntity(EntityType<? extends ThrownItemEntity> entity_type, World world) {
         super(entity_type, world);
-		this.world = world;
     }
     
     public BackpackProjectileEntity(World world, PlayerEntity player) {
-        //super(ModEntities.BACKPACK, player, world, new ItemStack(ModItems.BACKPACK), null);
-        super(ModEntities.BACKPACK, player, player.getVelocity(), world);
-		this.world = world;
+		super(ModEntities.BACKPACK, player, world, new ItemStack(ModItems.BACKPACK));
     }
+	
+	@Override
+	protected Item getDefaultItem() {
+		return ModItems.BACKPACK;
+	}
     
 	@Override
 	protected void onCollision(HitResult result) {
 		super.onCollision(result);
-	
 		
-		if (!this.world.isClient()) {
-			this.world.sendEntityStatus(this, (byte)3);
-			this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 9.0f, true, World.ExplosionSourceType.TNT);
+		if (!this.getEntityWorld().isClient()) {
+			this.getEntityWorld().sendEntityStatus(this, (byte)3);
+			this.getEntityWorld().createExplosion(this, this.getX(), this.getY(), this.getZ(), 9.0f, true, World.ExplosionSourceType.TNT);
 			this.discard();
 		}
-		
 	}
 }
--- src/main/java/alfredo/entities/custom/ThrustBombProjectileEntity.java
+++ src/main/java/alfredo/entities/custom/ThrustBombProjectileEntity.java
@@ -1,46 +1,37 @@
 package alfredo.entities.custom;
 
-//import alfredo.items.ModItems;
 import alfredo.entities.ModEntities;
-
-//import net.minecraft.item.ItemStack;
-
-import net.minecraft.util.hit.HitResult;
-//import net.minecraft.util.hit.BlockHitResult;
-//import net.minecraft.util.hit.EntityHitResult;
-
+import alfredo.items.ModItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.entity.projectile.ExplosiveProjectileEntity;
-
+import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.hit.HitResult;
 import net.minecraft.world.World;
-//import net.minecraft.world.explosion.Explosion;
 
-public class ThrustBombProjectileEntity extends ExplosiveProjectileEntity {
-	//private float rotation;
-	private World world;
-
-	public ThrustBombProjectileEntity(EntityType<? extends ExplosiveProjectileEntity> entity_type, World world) {
+public class ThrustBombProjectileEntity extends ThrownItemEntity {
+	public ThrustBombProjectileEntity(EntityType<? extends ThrownItemEntity> entity_type, World world) {
 		super(entity_type, world);
-		this.world = world;
 	}
 	
 	public ThrustBombProjectileEntity(World world, PlayerEntity player) {
-		//super(ModEntities.THRUST_BOMB, player, world, new ItemStack(ModItems.THRUST_BOMB), null);
-		super(ModEntities.THRUST_BOMB, player, player.getVelocity(), world);
-		this.world = world;
+		super(ModEntities.THRUST_BOMB, player, world, new ItemStack(ModItems.THRUST_BOMB));
+	}
+	
+	@Override
+	protected Item getDefaultItem() {
+		return ModItems.THRUST_BOMB;
 	}
 	
 	@Override
 	protected void onCollision(HitResult result) {
 		super.onCollision(result);
 	
-		
-		if (!this.world.isClient()) {
-			this.world.sendEntityStatus(this, (byte)3);
-			this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 3.0f, true, World.ExplosionSourceType.TNT);
+		if (!this.getEntityWorld().isClient()) {
+			this.getEntityWorld().sendEntityStatus(this, (byte)3);
+			this.getEntityWorld().createExplosion(this, this.getX(), this.getY(), this.getZ(), 3.0f, true, World.ExplosionSourceType.TNT);
 			this.discard();
 		}
-		
 	}
 }
